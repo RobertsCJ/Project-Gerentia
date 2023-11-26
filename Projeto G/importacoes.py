8409f05458727136065ce7ef6beb70d3865c2e3b
Wallyson Santos <contato.wallyson@hotmail.com>
2023-11-26 13:01:15 -0300

    Arquivo de importações + att de cache e  banco

---
+++ Projeto G/importacoes.py
@@ -0,0 +1,9 @@
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+import sys
+import uuid
+from datetime import *
+import sqlite3
+import pandas
+import secrets
