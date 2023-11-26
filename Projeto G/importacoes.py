41456151147dfa64e3cee771ac1fb314cebf3b09
Wallyson Santos <contato.wallyson@hotmail.com>
2023-11-26 18:15:59 -0300

    Head nova formatação

--- Projeto G/importacoes.py
+++ Projeto G/importacoes.py
@@ -1,3 +1,10 @@
+######################################################################
+#
+# POR: WALLYSON S. SOUZA, ROBERTO CARLOS e JONATAS N. FREITAS
+# PROJETO DESENVOLVIDO COM: Qt Designer e PySide6
+# V: 1.0.0
+#
+#####################################################################
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
